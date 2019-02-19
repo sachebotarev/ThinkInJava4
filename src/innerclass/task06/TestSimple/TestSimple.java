82f5cfc59aea64cb2241c4dbbf9ed57b3e11bcd3
Sergey A. Chebotarev <sachebotarev@gmail.com>
2019-02-19 09:23:41 +0300

    Update TestSimple.java

--- src/innerclass/task06/TestSimple/TestSimple.java
+++ src/innerclass/task06/TestSimple/TestSimple.java
@@ -1,19 +1,18 @@
 package innerclass.task06.TestSimple;
 
 import innerclass.task06.classpackage.SimpleClass;
+import innerclass.task06.interfacepackage.SimpleInterface;
 
 /**
  * Задача №6, Глава 10, Внутрениие классы, стр. 295
  * В третьем пакете создайте производный класс; внутри метода верните
  * объект защищенного внутреннего класса, преобразованный в интерфейс.
  */
+
 public class TestSimple extends SimpleClass{
-    public static void main(String[] args) {
-        TestSimple ts = new TestSimple();
-        ts.simpleInterface().simpleMethod();
-    }
 
-
-
+	  public SimpleInterface getSimpleInterface(){
+	        return new Inner();
+	    }
 
 }
